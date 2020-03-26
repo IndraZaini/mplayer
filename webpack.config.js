e8c74ffac6b5286a43c958173bd4ac661279a4e9
Indra Zaini <indra.zaini@dana.id>
2020-03-26 21:15:27 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
     entry: path.join(__dirname,'/src/index.js'),
     output: {
       path: path.resolve(__dirname,'dist'),
-      publicPath: '/assets/',
       filename: 'app.bundled.js',
     },
     devServer: {
