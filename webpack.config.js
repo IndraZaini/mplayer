c88dde3c3a9b8850f1a3d52a131f6b0f1b2b47fd
Indra Zaini <indra.zaini@dana.id>
2020-03-26 16:44:34 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+var path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
 
