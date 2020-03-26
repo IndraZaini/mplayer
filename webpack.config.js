8b15f1961bbbbb2b73b46b02e263c998a6f2c043
Indra Zaini <indra.zaini@dana.id>
2020-03-26 15:48:07 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
+
 module.exports = {
     mode: 'development',
-    entry: path.join(__dirname,'/src/index.js'),  
+    entry: path.join(__dirname,'/src/index.js'),
     output: {
       path: path.join(__dirname,'dist'),
       filename: 'app.bundled.js',
