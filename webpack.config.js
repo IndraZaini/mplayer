b423dcfbde47acc62257569b87d6c307ef689eba
Indra Zaini <indra.zaini@dana.id>
2020-03-26 15:45:09 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
         {
           test: /\.(png|jp(e*)g|svg|mp3)$/,
           use: [{
-            loader: 'file-loader',
+            loader: 'url-loader',
             options: {
               limit: 10000,
               name: 'assets/[hash]-[name].[ext]',
